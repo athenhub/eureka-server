a77be5690d02ce4d6980f654c65c2af1888c41cc
JKjiwon <jwkim.oa@gmail.com>
2025-11-12 13:03:13 +0900

    [CHORE] 패키지명 변경

--- src/main/java/com/athenhub/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/athenhub/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.atenhub.eurekaserver;
+package com.athenhub.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/athenhub/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/athenhub/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.atenhub.eurekaserver;
+package com.athenhub.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
